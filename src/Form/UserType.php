db1d1e19bce5e073494f5f63f7403bba1c95ba96
Kevin <kevin@Mac-mini-de-Kevin.local>
2024-04-24 11:18:45 +0200

    reset password

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -5,7 +5,11 @@ namespace App\Form;
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Validator\Constraints\Regex;
+use Symfony\Component\Validator\Constraints\Length;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
 
 class UserType extends AbstractType
 {
@@ -15,7 +19,26 @@ class UserType extends AbstractType
             ->add('Nom')
             ->add('Prenom')
             ->add('email')
-            //->add('password')
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les mots de passe ne correspondent pas.',
+                'mapped' => false,
+                'required' => false,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Répétez le mot de passe'],
+                'constraints' => [
+                    new Length([
+                        'min' => 8,
+                        'minMessage' => 'Le mot de passe doit faire au moins {{ limit }} caractères.',
+                        'max' => 4096,
+                        'maxMessage' => 'Le mot de passe ne doit pas dépasser {{ limit }} caractères.',
+                    ]),
+                    new Regex([
+                        'pattern' => '/^(?=.*\d)(?=.*[A-Z])(?=.*[@#$%])(?!.*(.)\1{2}).*[a-z]/m',
+                        'message' => 'Le mot de passe doit comprendre au moins 1 Majuscule, 1 Chiffre, 1 Caractère spécial.'
+                    ]),
+                ],
+            ])
             //->add('roles')
             ->add('Date_de_naissance')
             ->add('Adresse')
